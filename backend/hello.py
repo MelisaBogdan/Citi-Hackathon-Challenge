a74d85eff2323bd742ec506e32a848a515cc7ae1
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-08-02 14:36:03 +0100

    repo setup

---
+++ backend/hello.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import jsonify
+
+from matching_algo import get_all_interns
+
+app = Flask(__name__)
+app.debug= True
+@app.route("/")
+def hello_world():
+    return "<p>Hellooo World!</p>"
+
+@app.route("/api")
+def me_api():
+    interns = get_all_interns()
+    return jsonify([intern for intern in interns])
